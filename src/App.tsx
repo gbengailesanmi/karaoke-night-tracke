8cd33a394ed7f073d76ae74ca548e69b09b81815
Gbenga <145447898+gbengailesanmi@users.noreply.github.com>
2025-10-01 15:37:47 +0000

    Generated by Spark: Fix all reported errors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { HomePage } from '@/components/HomePage'
 import { EventPage } from '@/components/EventPage'
 import { useNavigation } from '@/hooks/useNavigation'
+import { Toaster } from '@/components/ui/sonner'
 
 function App() {
   const { currentPage, navigateTo, navigateHome, isHome } = useNavigation()
 
-  if (isHome) {
-    return <HomePage onNavigateToEvent={navigateTo} />
-  }
-
   return (
-    <EventPage 
-      eventId={currentPage} 
-      onNavigateHome={navigateHome}
-    />
+    <>
+      {isHome ? (
+        <HomePage onNavigateToEvent={navigateTo} />
+      ) : (
+        <EventPage 
+          eventId={currentPage} 
+          onNavigateHome={navigateHome}
+        />
+      )}
+      <Toaster />
+    </>
   )
 }
 
